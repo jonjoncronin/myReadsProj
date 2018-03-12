f591fab2cbba7baa4ceb919a9a389e304925ab15
Jon Cronin <jonjoncronin@gmail.com>
2018-03-11 18:44:37 -0700

    Add call to BooksAPI to get all books when app component mounts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  /**
+   * When BooksApp component mounts we need to call the BooksAPI to get the
+   * list of books that needs to be displayed by the App.
+   */
+  componentDidMount () {
+    BooksAPI.getAll()
+    .then((books) => {
+      this.setState({books})
+    })
+    .then(() => {
+      console.log(this.state.books)
+    })
   }
 
   render() {
