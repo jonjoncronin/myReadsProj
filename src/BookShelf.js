f5948eeec50a080eb94dcbbb637983ebe3d7bbc3
Jon Cronin <jonjoncronin@gmail.com>
2018-03-12 22:04:26 -0700

    Format code to fit within 80 columns

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -40,9 +40,19 @@ class BookShelf extends Component {
               <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                    <div className="book-cover"
+                      style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage:
+                          `url(${book.imageLinks.smallThumbnail})` }}>
+                    </div>
                     <div className="book-shelf-changer">
-                      <select defaultValue={filter} onChange={(event) => onUpdateShelf(book, event.target.value)}>
+                      <select
+                        defaultValue={filter}
+                        onChange={(event) => {
+                          onUpdateShelf(book, event.target.value)
+                        }}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
